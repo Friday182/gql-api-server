139da10e6cd1dbc8a2f3afc41714e25e0b508197
friday182 <friday182@gmail.com>
2019-12-06 23:22:26 +0000

    make first running server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,17 +2,49 @@ package main
 
 import (
 	log "log"
+	"time"
 
-	"github.com/wtlin1228/go-gql-server/internal/orm"
-	"github.com/wtlin1228/go-gql-server/pkg/server"
+	"github.com/gin-contrib/cors"
+	// "gql-api-server/internal/orm"
+
+	"github.com/gin-gonic/gin"
+	// "gql-api-server/internal/handlers"
 )
 
+var host, port string
+
+func init() {
+	// read configuration from configs/config.json
+	host = "localhost"
+	port = "8090"
+}
+
+// Config and start up the server
 func main() {
-	// Create a new ORM instance to send it to our server
-	orm, err := orm.Factory()
-	if err != nil {
-		log.Panic(err)
-	}
-	// Send: ORM instance
-	server.Run(orm)
+	endpoint := "http://" + host + ":" + port
+
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		// AllowOriginFunc:  func(origin string) bool { return origin == "http://localhost:3000" },
+		AllowOriginFunc:  func(origin string) bool { return true },
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// Handlers
+	// Simple keep-alive/ping handler
+	//r.GET("/heartbeat", handlers.Heartbeat())
+
+	// GraphQL handlers
+	//r.POST("/graghql", handlers.GraphqlHandler(orm))
+	log.Println("GraphQL @ " + endpoint + "/graphql")
+
+	// Run the server
+	// Inform the user where the server is listening
+	log.Println("Running @ " + endpoint)
+	// Print out and exit(1) to the OS if the server cannot run
+	log.Fatal(r.Run(host + ":" + port))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module gql-api-server
 
 go 1.13
+
+require (
+	github.com/gin-contrib/cors v1.3.0
+	github.com/gin-gonic/gin v1.4.0
+	github.com/wtlin1228/go-gql-server v0.0.0-20191008072347-dba0cc556a7e
+)
