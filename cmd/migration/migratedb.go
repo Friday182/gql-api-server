4b38565a03ee80b9abc10f9fd5c7e9bf8af58199
friday182 <friday182@gmail.com>
2020-01-06 00:11:07 +0000

    connect postgresql ok

--- cmd/migration/migratedb.go
+++ cmd/migration/migratedb.go
@@ -26,7 +26,13 @@ func testDb(db *gorm.DB) {
 	todo := models.Todo{Text: "test12345", Done: false}
 	db.Create(&todo)
 
+	//var existUser []models.User
+	//db.Find(&existUser)
+	//fmt.Println("exist todos: ", existUser[0].Todos[0].Text)
+
 	user := models.User{Email: "1@gmail.com", Password: "1234"}
+	var tmp = []models.Todo{todo}
+	user.Todos = tmp
 	db.Create(&user)
 
 	item := models.Todo{}
--- internal/graphql/resolvers/generated/resolver_gen.go
+++ internal/graphql/resolvers/generated/resolver_gen.go
@@ -4,11 +4,14 @@ import (
 	"context"
 
 	"github.com/friday182/gql-api-server/internal/graphql/generated"
+	"github.com/jinzhu/gorm"
 )
 
 // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
 
-type Resolver struct{}
+type Resolver struct{
+	Db *gorm.DB
+}
 
 func (r *Resolver) Mutation() generated.MutationResolver {
 	return &mutationResolver{r}
